2ddf84706e6ed4a4febba7ffb846164bbf3b8542
troystacer <trstacer@umich.edu>
2018-09-23 15:20:12 -0400

    Fixed warnings. still does not launch

--- Chatter/Chatter/ViewController.swift
+++ Chatter/Chatter/ViewController.swift
@@ -32,11 +32,15 @@ class ChattTableViewController: UITableViewController {
         request.httpMethod = "GET"
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let _ = data, error == nil else {
-            print("NETWORKING ERROR")
-            return }
+                print("NETWORKING ERROR")
+                self.refreshControl?.endRefreshing()
+                return
+            }
             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
                 print("HTTP STATUS: \(httpStatus.statusCode)")
-                return }
+                self.refreshControl?.endRefreshing()
+                return
+            }
             do {
                 var newChatts = [Chatt]()
                 let json = try JSONSerialization.jsonObject(with: data!) as! [String:Any]
@@ -47,17 +51,49 @@ class ChattTableViewController: UITableViewController {
                 }
                 self.chatts = newChatts
                 self.tableView.estimatedRowHeight = 140
-                self.tableView.rowHeight = UITableViewAutomaticDimension
+                self.tableView.rowHeight = UITableView.automaticDimension
                 self.tableView.reloadData()
+                self.refreshControl?.endRefreshing()
             }
             catch let error as NSError {
                 print(error) }
         }
         task.resume() }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatts.count
+        
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "ChattTableCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ChattTableCell else {
+            fatalError("The dequeued cell is not an instance of ChattTableCell")
+        }
+        let chatt = chatts[indexPath.row]
+        cell.usernameLabel.text = chatt.username
+        cell.usernameLabel.sizeToFit()
+        cell.messageLabel.text = chatt.message
+        cell.messageLabel.sizeToFit()
+        cell.timeStampLabel.text = chatt.timestamp
+        cell.timeStampLabel.sizeToFit()
+        return cell
+        
+    }
+    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+        self.refreshChatts() }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.refreshControl?.addTarget(self, action: #selector(ChattTableViewController.handleRefresh(_:)), for: UIControl.Event.valueChanged)
+        self.refreshChatts()
     }
     
     override func didReceiveMemoryWarning() {
@@ -73,6 +109,9 @@ class ComposeViewController: UIViewController {
     
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var messageTextView: UITextView!
+    @IBAction func submitButton(_ sender: Any) {
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
